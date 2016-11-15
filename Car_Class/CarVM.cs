915634224ccf690d92d5a7e846a75b91ce4eb2f9
Neville <nevil_dabre@yahoo.com>
2016-11-15 16:14:56 -0500

    Add project files.

---
+++ Car_Class/CarVM.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Class
+{
+    class CarVM : INotifyPropertyChanged
+    {
+        private string _error,_make,_year;
+        private int _speed;
+
+        public CarVM(string year, string make)
+        {
+            // TODO: Complete member initialization
+            this._year = year;
+            this._make = make;
+        }
+        
+        public string Error
+        {
+            get { return _error; }
+            set { _error = value; OnPropertyChanged(); }
+        }
+
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; OnPropertyChanged(); }
+        }
+
+        public string Year
+        {
+            get { return _year; }
+            set { _year = value; OnPropertyChanged(); }
+        }
+
+        public int Speed
+        {
+            get { return _speed; }
+            set { _speed = value; OnPropertyChanged(); }
+        }
+
+
+
+        public void Accelerate()
+        {
+            Error = string.Empty;
+            Speed = Speed + 5;
+        }
+
+        public void Brake()
+        {
+            Error = string.Empty;
+            if (Speed < 5)
+            {
+                Error = "Car Stopped";
+            }
+            else
+            {
+                Speed = Speed - 5;
+            }
+        }
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ Car_Class/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Car_Class
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CarVM cvm;
+        private const string Year = "2015";
+        private const string Make = "Ford";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cvm = new CarVM(Year, Make);
+            DataContext = cvm;
+        }
+
+        private void BtnAccelerate_Click(object sender, RoutedEventArgs e)
+        {
+            cvm.Accelerate();
+        }
+
+        private void BtnBrake_Click(object sender, RoutedEventArgs e)
+        {
+            cvm.Brake();
+        }
+
+
+    }
+}
